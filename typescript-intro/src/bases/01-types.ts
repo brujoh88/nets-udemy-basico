ed8f9542239af9970beda2b6b7a49e02768eea05
Gus - MVC <gustavotiseira@gmail>
2025-07-31 07:20:57 -0300

    12

--- typescript-intro/src/bases/01-types.ts
+++ typescript-intro/src/bases/01-types.ts
@@ -1,3 +1,14 @@
-export const name = "Gustavo"
+export let name = "Gustavo"
 export const age: number = 37
 export const isValid: boolean = true
+
+name = "Hernan"
+
+export const templateString = `Este es un
+string
+puede tener '
+o tambien "
+variables con \$\{\}
+boolean ${isValid}`
+
+console.log(templateString)
